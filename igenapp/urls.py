2f55233090e2276ec571d989d74db2cf72926405
kimnovak <kimnovak187@gmail.com>
2018-01-13 02:52:21 +0100

    added simple row insert into database for test purposes

--- igenapp/urls.py
+++ igenapp/urls.py
@@ -3,4 +3,5 @@ from igenapp import views
 
 urlpatterns = [
                url(r'^$', views.index, name='index'),
+               url(r'(?P<tea>\w+)/$', views.insert, name='insert'),
               ]
--- igenapp/views.py
+++ igenapp/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from igenapp.models import Example
+
 
 def index(request):
-    return HttpResponse("aaaa")
+    return render(request, 'igenapp/index.html')
+
+def insert(request, tea):
+    #upis u bazu
+    Example.objects.create(text=tea)
+    #ispise sve upisane objekte
+    return HttpResponse(Example.objects.all())
