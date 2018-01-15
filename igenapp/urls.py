f6ce9c448a93e7d81c867e9ed50c97df400e9078
Danilo-Zekovic <danilozeka93@gmail.com>
2018-01-15 12:37:02 +0100

    start done

--- igenapp/urls.py
+++ igenapp/urls.py
@@ -4,7 +4,7 @@ from igenapp import views
 urlpatterns = [
                #url(r'^$', views.index, name='index'),
                #url(r'(?P<tea>\w+)/$', views.insert, name='insert'),
-    url(r'^home/$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^wiki/$', views.wiki, name='wiki'),
     url(r'^issues/$', views.issues, name='issues'),
     url(r'^commits/$', views.commits, name='commits'),
