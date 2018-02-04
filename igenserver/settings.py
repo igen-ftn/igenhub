1853c8d6e215a3f327af7bb7efe12fbb5c4aefc6
kimnovak <kimnovak187@gmail.com>
2018-02-04 09:52:23 +0100

    settings izmene

--- igenserver/settings.py
+++ igenserver/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'igenapp/static'),
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'igenapp/media')
