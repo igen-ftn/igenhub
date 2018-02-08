ba77ea9a45e534baaa03e728d87466c1f5baadb6
Ana Marojević <ana94ns@hotmail.com>
2018-02-08 14:19:33 +0100

    Fixed test bug #27

--- igenapp/tests.py
+++ igenapp/tests.py
@@ -12,10 +12,11 @@ class IssueTests(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.user = User.objects.create(username="user", password="user", email="user@gmail.com")
+        self.repository = Repository.objects.create(author=self.user, repo_name="repo",
+                                                    owner_name=self.user.username, type='L')
         self.issue = Issue.objects.create(title="Some Issue", text="This is first issue", ordinal=100,
-                                          date=timezone.now(), status='O', user=self.user)
-        self.repository = Repository.objects.create(author=self.issue.user, repo_name="repo",
-                                                    owner_name=self.issue.user.username, type='L')
+                                          date=timezone.now(), status='O', user=self.user, repository=self.repository)
+
 
     """Creating new issue test"""
     def test_create_issue(self):
@@ -42,4 +43,11 @@ class IssueTests(TestCase):
         data = {'title': "", 'text': "This is form body", 'milestone': "null"}
         form = IssueForm(data=data)
         self.assertFalse(form.is_valid())
+
+    # """User acitvity test"""
+    # def test_activity(self):
+    #     response = self.client.get('polls:index')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "No polls are available.")
+    #     self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
